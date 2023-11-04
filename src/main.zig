ddc8bb9b5f13e8534f2ac161ea14dfed485db29d
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-11-04 10:05:39 +0100

    Implement a read/print loop

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.debug.warn("All your codebase are belong to us.\n", .{});
+    const child = try std.ChildProcess.run(.{
+        .allocator = std.heap.page_allocator,
+        .argv = &[_][]const u8{ "echo", "hello", "world" },
+    });
+    std.debug.print("Child output: {s}", .{child.stdout});
+
+    const in = std.io.getStdIn();
+    var buf = std.io.bufferedReader(in.reader());
+    var r = buf.reader();
+
+    var msg_buf: [4096]u8 = undefined;
+    while (true) {
+        std.debug.print(">> ", .{});
+        var msg = try r.readUntilDelimiterOrEof(&msg_buf, '\n');
+        if (msg) |m| {
+            std.debug.print("Got: {s}\n", .{m});
+        }
+    }
 }
