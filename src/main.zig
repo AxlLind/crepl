becbca0ba52824c0c3b16dba93dfe8c6d6258d8a
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-11-05 13:48:23 +0100

    Remove unnecessary helper functions

--- src/main.zig
+++ src/main.zig
@@ -17,31 +17,16 @@ fn c_source(
     return std.fmt.allocPrint(allocator, SOURCE, .{ .includes = include_str, .exprs = expr_str, .expr = expr });
 }
 
-fn compile_cfile(path: []const u8, binpath: []const u8, allocator: std.mem.Allocator) anyerror!std.process.Child.RunResult {
-    return try std.ChildProcess.run(.{
-        .allocator = allocator,
-        .argv = &[_][]const u8{ "gcc", path, "-o", binpath },
-    });
-}
-
-fn run_binary(path: []const u8, allocator: std.mem.Allocator) anyerror!std.process.Child.RunResult {
-    return try std.ChildProcess.run(.{
-        .allocator = allocator,
-        .argv = &[_][]const u8{path},
-    });
+fn sig_handler(sig: c_int) callconv(.C) void {
+    if (sig != std.os.SIG.INT) {
+        std.process.exit(1);
+    }
+    std.debug.print("Use Ctrl+D to exit...\n>> ", .{});
 }
 
 fn installSignalHandler() !void {
-    const internal_handler = struct {
-        fn handle(sig: c_int) callconv(.C) void {
-            if (sig != std.os.SIG.INT) {
-                std.process.exit(1);
-            }
-            std.debug.print("Use Ctrl+D to exit...\n>> ", .{});
-        }
-    };
     const action = std.os.Sigaction{
-        .handler = .{ .handler = internal_handler.handle },
+        .handler = .{ .handler = sig_handler },
         .mask = std.os.empty_sigset,
         .flags = 0,
     };
@@ -56,9 +41,8 @@ pub fn main() anyerror!void {
     var cfile = try std.fs.createFileAbsolute(TMPFILE, .{});
     defer {
         cfile.close();
-        for ([_][]const u8{ TMPFILE, BINFILE }) |file| {
-            std.fs.deleteFileAbsolute(file) catch {};
-        }
+        std.fs.deleteFileAbsolute(TMPFILE) catch {};
+        std.fs.deleteFileAbsolute(BINFILE) catch {};
     }
 
     var includes = std.ArrayList([]u8).init(arena.allocator());
@@ -72,34 +56,31 @@ pub fn main() anyerror!void {
         std.debug.print(">> ", .{});
         var msg = try r.readUntilDelimiterOrEof(&msg_buf, '\n') orelse break;
 
-        const csrc = try c_source(includes.items, exprs.items, msg, std.heap.page_allocator);
-        defer std.heap.page_allocator.free(csrc);
+        var tmp_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer tmp_arena.deinit();
 
         try cfile.seekTo(0);
-        try cfile.writeAll(csrc);
-        const compile_result = try compile_cfile(TMPFILE, BINFILE, std.heap.page_allocator);
-        defer std.heap.page_allocator.free(compile_result.stderr);
-        defer std.heap.page_allocator.free(compile_result.stdout);
-
+        try cfile.writeAll(try c_source(includes.items, exprs.items, msg, tmp_arena.allocator()));
+        const compile_result = try std.ChildProcess.run(.{
+            .allocator = tmp_arena.allocator(),
+            .argv = &[_][]const u8{ "gcc", TMPFILE, "-o", BINFILE },
+        });
         if (compile_result.term.Exited != 0) {
-            std.debug.print("{s}\n", .{compile_result.stderr});
+            std.debug.print("{s}", .{compile_result.stderr});
             continue;
         }
 
-        const run_result = try run_binary(BINFILE, std.heap.page_allocator);
-        defer std.heap.page_allocator.free(run_result.stderr);
-        defer std.heap.page_allocator.free(run_result.stdout);
+        const run_result = try std.ChildProcess.run(.{
+            .allocator = tmp_arena.allocator(),
+            .argv = &[_][]const u8{BINFILE},
+        });
         if (run_result.term.Exited != 0) {
-            std.debug.print("Exited with {}!\n", .{run_result.term.Exited});
-            if (run_result.stderr.len > 0) {
-                std.debug.print("{s}\n", .{run_result.stderr});
-            }
+            std.debug.print("Exited with {}!\n{s}", .{ run_result.term.Exited, run_result.stderr });
             continue;
         }
 
         std.debug.print("{s}", .{run_result.stdout});
         try exprs.append(try arena.allocator().dupe(u8, msg));
     }
-
     std.debug.print("\n", .{});
 }
