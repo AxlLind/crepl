0372da6c7452e220b5370cfa5b794870e0c3c6ec
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-11-05 11:28:08 +0100

    Implement basic repl loop

--- src/main.zig
+++ src/main.zig
@@ -1,55 +1,80 @@
 const std = @import("std");
 
 const SOURCE = @embedFile("./source.c");
+const TMPFILE = "/tmp/crepl.c";
+const BINFILE = "/tmp/crepl.bin";
 
 fn c_source(
     includes: []const []const u8,
     exprs: []const []const u8,
+    expr: []const u8,
     allocator: std.mem.Allocator,
 ) ![]u8 {
     const include_str = try std.mem.join(allocator, "\n", includes);
     defer allocator.free(include_str);
     const expr_str = try std.mem.join(allocator, "\n  ", exprs);
     defer allocator.free(expr_str);
-    return std.fmt.allocPrint(allocator, SOURCE, .{ include_str, expr_str });
+    return std.fmt.allocPrint(allocator, SOURCE, .{ .includes = include_str, .exprs = expr_str, .expr = expr });
+}
+
+fn compile_cfile(path: []const u8, binpath: []const u8, allocator: std.mem.Allocator) anyerror!std.process.Child.RunResult {
+    return try std.ChildProcess.run(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{ "gcc", path, "-o", binpath },
+    });
+}
+
+fn run_binary(path: []const u8, allocator: std.mem.Allocator) anyerror!std.process.Child.RunResult {
+    return try std.ChildProcess.run(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{path},
+    });
 }
 
 pub fn main() anyerror!void {
-    const allocator = std.heap.page_allocator;
-    const child = try std.ChildProcess.run(.{
-        .allocator = allocator,
-        .argv = &[_][]const u8{ "echo", "hello", "world" },
-    });
-    std.debug.print("Child output: {s}", .{child.stdout});
-    allocator.free(child.stdout);
-    allocator.free(child.stderr);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    var cfile = try std.fs.createFileAbsolute(TMPFILE, .{});
+    defer {
+        cfile.close();
+        for ([_][]const u8{ TMPFILE, BINFILE }) |file| {
+            std.fs.deleteFileAbsolute(file) catch {};
+        }
+    }
+
+    var includes = std.ArrayList([]u8).init(arena.allocator());
+    var exprs = std.ArrayList([]u8).init(arena.allocator());
 
     const in = std.io.getStdIn();
     var buf = std.io.bufferedReader(in.reader());
     var r = buf.reader();
-
-    var includes = std.ArrayList([]u8).init(allocator);
-    defer {
-        for (includes.items) |include| {
-            allocator.free(include);
-        }
-        includes.deinit();
-    }
-    var exprs = std.ArrayList([]u8).init(allocator);
-    defer {
-        for (exprs.items) |expr| {
-            allocator.free(expr);
-        }
-        exprs.deinit();
-    }
-
     var msg_buf: [4096]u8 = undefined;
     while (true) {
         std.debug.print(">> ", .{});
         var msg = try r.readUntilDelimiterOrEof(&msg_buf, '\n') orelse break;
-        std.debug.print("Got: {s}\n", .{msg});
-        try exprs.append(try allocator.dupe(u8, msg));
+
+        const csrc = try c_source(includes.items, exprs.items, msg, std.heap.page_allocator);
+        defer std.heap.page_allocator.free(csrc);
+
+        try cfile.seekTo(0);
+        try cfile.writeAll(csrc);
+        const compile_result = try compile_cfile(TMPFILE, BINFILE, std.heap.page_allocator);
+        defer std.heap.page_allocator.free(compile_result.stderr);
+        defer std.heap.page_allocator.free(compile_result.stdout);
+
+        if (compile_result.term.Exited != 0) {
+            std.debug.print("{s}\n", .{compile_result.stderr});
+            continue;
+        }
+        try exprs.append(try arena.allocator().dupe(u8, msg));
+
+        const run_result = try run_binary(BINFILE, std.heap.page_allocator);
+        defer std.heap.page_allocator.free(run_result.stderr);
+        defer std.heap.page_allocator.free(run_result.stdout);
+        std.debug.print("(result {})\n{s}{s}", .{ run_result.term.Exited, run_result.stdout, run_result.stderr });
     }
+
     std.debug.print("\n", .{});
     std.debug.print("includes:\n", .{});
     for (includes.items) |include| {
