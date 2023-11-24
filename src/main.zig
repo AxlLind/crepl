81ed5f7df4d863e5d9cc64505da3a58aa4dadc2f
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-11-24 17:37:46 +0100

    Print expression values

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,28 @@ const SOURCE = @embedFile("./source.c");
 const TMPFILE = "/tmp/crepl.c";
 const BINFILE = "/tmp/crepl.bin";
 
+fn expr_print_str(expr: []const u8, tpe: c_uint, allocator: std.mem.Allocator) !?[]const u8 {
+    const fmt = switch (tpe) {
+        clang.CXType_Bool => "d",
+        clang.CXType_UChar => "hhu",
+        clang.CXType_UShort => "hu",
+        clang.CXType_UInt => "u",
+        clang.CXType_ULong => "lu",
+        clang.CXType_ULongLong => "llu",
+        clang.CXType_SChar => "hhd",
+        clang.CXType_Short => "hd",
+        clang.CXType_Int => "d",
+        clang.CXType_Long => "ld",
+        clang.CXType_LongLong => "lld",
+        clang.CXType_Float => "f",
+        clang.CXType_Double => "f",
+        clang.CXType_LongDouble => "Lf",
+        clang.CXType_Pointer => "p",
+        else => return null,
+    };
+    return try std.fmt.allocPrint(allocator, "printf(\"%{s}\\n\", {s});", .{ fmt, std.mem.trimRight(u8, expr, ";") });
+}
+
 fn get_last_child(c: clang.CXCursor) ?clang.CXCursor {
     const f = struct {
         pub fn visit(cursor: clang.CXCursor, _: clang.CXCursor, data: clang.CXClientData) callconv(.C) clang.enum_CXChildVisitResult {
@@ -45,6 +67,16 @@ fn c_source(
     return std.fmt.allocPrint(allocator, SOURCE, .{ .includes = include_str, .exprs = expr_str, .expr = expr });
 }
 
+fn write_and_compile(src: []const u8, allocator: std.mem.Allocator) !std.ChildProcess.RunResult {
+    var cfile = try std.fs.createFileAbsolute(TMPFILE, .{ .truncate = true });
+    defer cfile.close();
+    try cfile.writeAll(src);
+    return std.ChildProcess.run(.{
+        .allocator = allocator,
+        .argv = &.{ "gcc", TMPFILE, "-o", BINFILE },
+    });
+}
+
 fn sig_handler(_: c_int) callconv(.C) void {
     std.os.close(std.os.STDIN_FILENO);
 }
@@ -56,11 +88,8 @@ pub fn main() anyerror!void {
         .flags = 0,
     }, null);
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-
-    var cfile = try std.fs.createFileAbsolute(TMPFILE, .{});
     defer {
-        cfile.close();
+        arena.deinit();
         std.fs.deleteFileAbsolute(TMPFILE) catch {};
         std.fs.deleteFileAbsolute(BINFILE) catch {};
     }
@@ -84,13 +113,12 @@ pub fn main() anyerror!void {
             continue;
         }
 
-        try cfile.seekTo(0);
-        try cfile.writeAll(try c_source(includes.items, exprs.items, msg, tmp_arena.allocator()));
-        const compile_result = try std.ChildProcess.run(.{
-            .allocator = tmp_arena.allocator(),
-            .argv = &.{ "gcc", TMPFILE, "-o", BINFILE },
-        });
+        var compile_result = try write_and_compile(
+            try c_source(includes.items, exprs.items, msg, tmp_arena.allocator()),
+            tmp_arena.allocator(),
+        );
         if (compile_result.term.Exited != 0) {
+            std.debug.print("{s}", .{try c_source(includes.items, exprs.items, msg, tmp_arena.allocator())});
             std.debug.print("{s}", .{compile_result.stderr});
             continue;
         }
@@ -103,11 +131,20 @@ pub fn main() anyerror!void {
         var main_block_cursor = get_last_child(main_fn_cursor) orelse unreachable;
         var new_expr_cursor = get_last_child(main_block_cursor) orelse unreachable;
 
-        var name = std.mem.span(clang.clang_getCString(clang.clang_getCursorSpelling(new_expr_cursor)));
-        var cursor_kind = clang.clang_getCString(clang.clang_getTypeKindSpelling(clang.clang_getCursorKind(new_expr_cursor)));
-        var kind = if (cursor_kind == null) "" else std.mem.span(cursor_kind);
-        var tpe = std.mem.span(clang.clang_getCString(clang.clang_getTypeKindSpelling(clang.clang_getCursorType(new_expr_cursor).kind)));
-        std.debug.print("found '{s}': kind={s}, tpe={s}, is_expr={}\n", .{ name, kind, tpe, clang.clang_isExpression(clang.clang_getCursorKind(new_expr_cursor)) != 0 });
+        var kind = clang.clang_getCursorKind(new_expr_cursor);
+        var tpe = clang.clang_getCursorType(new_expr_cursor);
+        if (clang.clang_isExpression(kind) != 0) {
+            if (try expr_print_str(msg, tpe.kind, tmp_arena.allocator())) |s| {
+                var comp_result = try write_and_compile(
+                    try c_source(includes.items, exprs.items, s, tmp_arena.allocator()),
+                    tmp_arena.allocator(),
+                );
+                if (comp_result.term.Exited != 0) {
+                    std.debug.print("{s}", .{comp_result.stderr});
+                    continue;
+                }
+            }
+        }
 
         const run_result = try std.ChildProcess.run(.{
             .allocator = tmp_arena.allocator(),
