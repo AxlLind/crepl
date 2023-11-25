cb648c971f9af45a949ade584199c9a0eb2b0534
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-11-25 09:57:05 +0100

    Use const when possible

--- src/main.zig
+++ src/main.zig
@@ -9,12 +9,17 @@ const Command = enum {
     printSource,
     quit,
 
+    const mapping = [_]struct { []const []const u8, Command }{
+        .{ &.{"print_source"}, .printSource },
+        .{ &.{ "q", "quit" }, .quit },
+    };
+
     fn parse(s: []const u8) ?Command {
-        if (std.mem.eql(u8, s, "print_source")) {
-            return .printSource;
-        }
-        if (std.mem.eql(u8, s, "q") or std.mem.eql(u8, s, "quit")) {
-            return .quit;
+        for (mapping) |t| {
+            for (t.@"0") |cmd| {
+                if (std.mem.eql(u8, s, cmd))
+                    return t.@"1";
+            }
         }
         return null;
     }
@@ -98,25 +103,25 @@ pub fn main() anyerror!void {
     var exprs = std.ArrayList([]u8).init(arena.allocator());
 
     const in = std.io.getStdIn();
-    var buf = std.io.bufferedReader(in.reader());
-    var r = buf.reader();
+    var r = std.io.bufferedReader(in.reader());
     var msg_buf: [4096]u8 = undefined;
     while (true) {
         std.debug.print(">> ", .{});
-        var msg = r.readUntilDelimiterOrEof(&msg_buf, '\n') catch break orelse break;
+        const expr = r.reader().readUntilDelimiterOrEof(&msg_buf, '\n') catch break orelse break;
 
         var tmp_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        var tmp_alloc = tmp_arena.allocator();
         defer tmp_arena.deinit();
 
-        if (msg.len > 1 and msg[0] == ':') {
-            const cmdstr = std.mem.trim(u8, msg[1..], " \t");
+        if (expr.len > 1 and expr[0] == ':') {
+            const cmdstr = std.mem.trim(u8, expr[1..], " \t");
             const cmd = Command.parse(cmdstr) orelse {
                 std.debug.print("Unrecognized builtin command '{s}'\n", .{cmdstr});
                 continue;
             };
             switch (cmd) {
                 .printSource => {
-                    const src = try c_source(includes.items, exprs.items, "// next expression", tmp_arena.allocator());
+                    const src = try c_source(includes.items, exprs.items, "// next expr", tmp_alloc);
                     std.debug.print("{s}", .{src});
                 },
                 .quit => break,
@@ -124,30 +129,30 @@ pub fn main() anyerror!void {
             continue;
         }
 
-        var compile_result = try write_and_compile(
-            try c_source(includes.items, exprs.items, msg, tmp_arena.allocator()),
-            tmp_arena.allocator(),
+        const compile_result = try write_and_compile(
+            try c_source(includes.items, exprs.items, expr, tmp_alloc),
+            tmp_alloc,
         );
         if (compile_result.term.Exited != 0) {
             std.debug.print("{s}", .{compile_result.stderr});
             continue;
         }
 
-        var index = clang.clang_createIndex(0, 0);
+        const index = clang.clang_createIndex(0, 0);
         defer clang.clang_disposeIndex(index);
-        var unit = clang.clang_parseTranslationUnit(index, TMPFILE, null, 0, null, 0, 0);
+        const unit = clang.clang_parseTranslationUnit(index, TMPFILE, null, 0, null, 0, 0);
         defer clang.clang_disposeTranslationUnit(unit);
-        var main_fn_cursor = get_last_child(clang.clang_getTranslationUnitCursor(unit)) orelse unreachable;
-        var main_block_cursor = get_last_child(main_fn_cursor) orelse unreachable;
-        var new_expr_cursor = get_last_child(main_block_cursor) orelse unreachable;
+        const main_fn_cursor = get_last_child(clang.clang_getTranslationUnitCursor(unit)) orelse unreachable;
+        const main_block_cursor = get_last_child(main_fn_cursor) orelse unreachable;
+        const new_expr_cursor = get_last_child(main_block_cursor) orelse unreachable;
 
-        var kind = clang.clang_getCursorKind(new_expr_cursor);
-        var tpe = clang.clang_getCursorType(new_expr_cursor);
+        const kind = clang.clang_getCursorKind(new_expr_cursor);
+        const tpe = clang.clang_getCursorType(new_expr_cursor);
         if (clang.clang_isExpression(kind) != 0) {
-            if (try expr_print_str(msg, tpe.kind, tmp_arena.allocator())) |s| {
-                var comp_result = try write_and_compile(
-                    try c_source(includes.items, exprs.items, s, tmp_arena.allocator()),
-                    tmp_arena.allocator(),
+            if (try expr_print_str(expr, tpe.kind, tmp_alloc)) |s| {
+                const comp_result = try write_and_compile(
+                    try c_source(includes.items, exprs.items, s, tmp_alloc),
+                    tmp_alloc,
                 );
                 if (comp_result.term.Exited != 0) {
                     std.debug.print("{s}", .{comp_result.stderr});
@@ -157,7 +162,7 @@ pub fn main() anyerror!void {
         }
 
         const run_result = try std.ChildProcess.run(.{
-            .allocator = tmp_arena.allocator(),
+            .allocator = tmp_alloc,
             .argv = &.{BINFILE},
         });
         if (run_result.term.Exited != 0) {
@@ -166,7 +171,7 @@ pub fn main() anyerror!void {
         }
 
         std.debug.print("{s}", .{run_result.stdout});
-        try exprs.append(try arena.allocator().dupe(u8, msg));
+        try exprs.append(try arena.allocator().dupe(u8, expr));
     }
     std.debug.print("\n", .{});
 }
